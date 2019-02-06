6ed8f9499fe4401d89ce7d6dd772752b358b955d
Kelang <Kelang@users.noreply.github.com>
2019-02-05 20:00:13 -0500

    Structure de la page Index et styles

---
+++ js/BurgerMenu.js
@@ -0,0 +1,26 @@
+$("#wrapper").click(function(){
+
+    $(".menu").toggleClass("BurgerMenuAnim");
+    $(".MenuContainer").toggleClass("MenuAnimEntrer");
+    $(".navLink").toggleClass("navContentAnim");
+    
+
+
+
+
+    /*
+    if($(".MenuContainer").hasClass("MenuAnimSortie")){
+        $(".MenuContainer").removeClass("MenuAnimSortie");
+        $(".MenuContainer").addClass("MenuAnimEntrer");
+    }
+    */
+   
+
+
+
+    
+
+
+
+
+});
