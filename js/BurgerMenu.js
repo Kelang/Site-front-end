0209e5abd6d7a7cf07299c40ffcc72e7b9eb8055
Kelang <Kelang@users.noreply.github.com>
2019-02-05 21:56:09 -0500

    Menu burger fonctionnel

--- js/BurgerMenu.js
+++ js/BurgerMenu.js
@@ -1,26 +1,38 @@
-$("#wrapper").click(function(){
+   $("#wrapper").click(function(){
 
     $(".menu").toggleClass("BurgerMenuAnim");
-    $(".MenuContainer").toggleClass("MenuAnimEntrer");
     $(".navLink").toggleClass("navContentAnim");
-    
+    $('input.checkBurger').prop("checked", !$('input.checkBurger').prop("checked"));
 
 
-
-
-    /*
-    if($(".MenuContainer").hasClass("MenuAnimSortie")){
+    if ($('input.checkBurger').is(':checked')) {
         $(".MenuContainer").removeClass("MenuAnimSortie");
         $(".MenuContainer").addClass("MenuAnimEntrer");
+        console.log("COCHER");
     }
-    */
-   
 
-
-
-    
-
-
+    else if($('input.checkBurger'). prop("checked") == false){
+        $(".MenuContainer").removeClass("MenuAnimEntrer");
+    $(".MenuContainer").addClass("MenuAnimSortie");
+    console.log("DECOCHER");
+    }
 
 
 });
+    
+/*
+if($('input.checkBurger').prop('checked',true)) {
+    //$(".MenuContainer").removeClass("MenuAnimSortie");
+   // $(".MenuContainer").addClass("MenuAnimEntrer");
+
+
+    console.log("COCHER");
+   }
+
+   else {
+    //$(".MenuContainer").removeClass("MenuAnimEntrer");
+    //$(".MenuContainer").addClass("MenuAnimSortie");
+    console.log("DECOCHER");
+
+   }
+   */
