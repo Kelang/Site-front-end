20d886e4e1046cedf92a67550e89a55f921a96ad
Kelang <Kelang@users.noreply.github.com>
2019-02-09 18:48:10 -0500

    Style version 1

--- js/BurgerMenu.js
+++ js/BurgerMenu.js
@@ -1,16 +1,18 @@
-   $("#wrapper").click(function(){
+   $("#BurgerMenuWrapper").click(function(){
 
     $(".menu").toggleClass("BurgerMenuAnim");
     $(".navLink").toggleClass("navContentAnim");
     $('input.checkBurger').prop("checked", !$('input.checkBurger').prop("checked"));
 
 
+    //Si le bouton checkbox est coché alors on change le width du side menu
     if ($('input.checkBurger').is(':checked')) {
         $(".MenuContainer").removeClass("MenuAnimSortie");
         $(".MenuContainer").addClass("MenuAnimEntrer");
         console.log("COCHER");
     }
 
+    //Si le bouton checkbox est décoché alors on change le width du side menu
     else if($('input.checkBurger'). prop("checked") == false){
         $(".MenuContainer").removeClass("MenuAnimEntrer");
     $(".MenuContainer").addClass("MenuAnimSortie");
